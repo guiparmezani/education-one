5c07fc289720b31a29db0ac8a75c0d0f2b7193d5
Guilherme Parmezani <g@parmezani.com>
2017-11-29 17:36:36 -0600

    Careers page dev progress

--- wp-content/themes/educationone/js/main.js
+++ wp-content/themes/educationone/js/main.js
@@ -1,4 +1,15 @@
 ;(function($, window, document, undefined) { //jquery reference
 
+$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+	var windowHeight = $(window).height() - 15;
+
+	if ($('.parallax-bg').length > 0) {
+
+	  var parallaxFactorPositive = (scroll * 0.08) + 40;
+	  $(".parallax-bg").css('background-position', '50% ' + parallaxFactorPositive + '%');
+	}
+
+});
 
 })(window.Zepto || window.jQuery, window, document);
--- wp-content/themes/educationone/js/theme.js
+++ wp-content/themes/educationone/js/theme.js
@@ -3890,5 +3890,16 @@ return exports;
 
 ;(function($, window, document, undefined) { //jquery reference
 
+$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+	var windowHeight = $(window).height() - 15;
+
+	if ($('.parallax-bg').length > 0) {
+
+	  var parallaxFactorPositive = (scroll * 0.08) + 40;
+	  $(".parallax-bg").css('background-position', '50% ' + parallaxFactorPositive + '%');
+	}
+
+});
 
 })(window.Zepto || window.jQuery, window, document);
