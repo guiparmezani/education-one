a386426bcf9232bdf2528e58cdf3c09420a9e00e
Guilherme Parmezani <g@parmezani.com>
2019-06-05 14:06:06 -0300

    Added GTM code into sites head tag

--- wp-content/themes/educationone/header.php
+++ wp-content/themes/educationone/header.php
@@ -23,11 +23,23 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:400,500|Barlow:300i,400,700,900" rel="stylesheet">
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WJLC5H9');</script>
+	<!-- End Google Tag Manager -->
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJLC5H9"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 
 <div class="hfeed site" id="page">
 
